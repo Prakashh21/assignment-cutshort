6f8c528c3626743fdecefd0fb593dd6acfd90714
Prakash <prakashope@gmail.com>
2022-09-08 02:26:57 +0530

    linted

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -33,55 +33,51 @@ function Stepper({ steps, currentStep }) {
   };
   useEffect(() => {
     const stepsState = steps.map((_, index) => {
-      let obj = { completed: false, selected: index === 0 ? true : false };
+      const obj = { completed: false, selected: index === 0 };
       return obj;
     });
-   
+
     stepRef.current = stepsState;
     const current = updateStep(currentStep - 1, stepRef.current);
 
     setNewStep(current);
   }, [steps, currentStep]);
 
-  const displaySteps = newStep.map((step, index) => {
-    return (
-      <>
-        <div
-          key={index}
-          className={
+  const displaySteps = newStep.map((step, index) => (
+    <div
+      key={index}
+      className={
             index !== newStep.length - 1
               ? 'w-full flex items-center '
               : 'flex items-center'
           }
+    >
+      <div className="relative flex flex-col items-center text-teal-600">
+        <div
+          className={`rounded-full transition duration-400 ease-in-out border-2 border-gray-300 h-9 w-9 flex items-center justify-center py-3 ${
+            step.completed ? 'bg-[#664de5] text-white border-[#664de5]' : ''
+          }`}
         >
-          <div className='relative flex flex-col items-center text-teal-600'>
-            <div
-              className={`rounded-full transition duration-400 ease-in-out border-2 border-gray-300 h-9 w-9 flex items-center justify-center py-3 ${
-                step.completed ? 'bg-[#664de5] text-white border-[#664de5]' : ''
-              }`}
-            >
-              {index + 1}
-              {/* Display Number */}
-            </div>
-          </div>
-          <div
-            className={`flex-auto w-full  transition duration-400 ease-in-out bg-gray-400 h-[1.6px] `}
-          >
-            {console.log(step)}
-            <div
-              className={`h-full  bg-[#664de5] ${
-                step.selected ? `w-2/4` : step.completed ? 'w-full' : 'w-0'
-              }`}
-            ></div>
-            {/* Display Line */}
-          </div>
+          {index + 1}
+          {/* Display Number */}
         </div>
-      </>
-    );
-  });
+      </div>
+      <div
+        className="flex-auto w-full  transition duration-400 ease-in-out bg-gray-400 h-[1.6px] "
+      >
+
+        <div
+          className={`h-full  bg-[#664de5] ${
+            step.selected ? 'w-2/4' : step.completed ? 'w-full' : 'w-0'
+          }`}
+        />
+        {/* Display Line */}
+      </div>
+    </div>
+  ));
 
   return (
-    <div className='mx-4 py-4 flex justify-between items-center'>
+    <div className="mx-4 py-4 flex justify-between items-center">
       {displaySteps}
     </div>
   );
